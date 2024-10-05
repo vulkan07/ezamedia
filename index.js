0b27d6d2e35bea32b8bfd24d490d7644739fa017
Barni <olahbozobarnabas@gmail.com>
2024-10-06 01:05:24 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function toggleNavBar() {
+	document.querySelector('.navbar').classList.toggle('active');
+	document.querySelector('.navbar-btn').classList.toggle('active');
+}
+function playVideo() {
+	const button = document.querySelector('.btn');
+	const video = document.getElementById('video');
+	button.classList.add('hidden');
+	video.classList.remove('hidden');
+	video.volume = 0.4;
+	video.play();
+
+	document.getElementById('video').addEventListener('ended',() => {
+		document.querySelector('.btn').classList.remove("hidden");
+		document.getElementById('video').classList.add("hidden");
+	},false);
+}
+
